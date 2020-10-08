2089a1fc3f95636eaef10479fc80e9bd112526b4
Travis Stevenson <thetechtravis@outlook.com>
2020-10-08 15:05:26 -0500

    Working automation, broken CSS flexbox.

--- scripts/Fish.js
+++ scripts/Fish.js
@@ -1,15 +1,21 @@
 /**
  *  Fish which renders individual fish objects as HTML
  */
-export const Fish = (fish) => {
+export const Fish = (fishObj) => {
     return `
-        <section class="fish card">
-            <div><img  class="fish__image image--card" src="${fish.image}" /></div>
-            <div class="fish__name">${fish.name}</div>
-            <div class="fish__species">${fish.species}</div>
-            <div class="fish__length">${fish.length}</div>
-            <div class="fish__location">${fish.location}</div>
-            <div class="fish__diet">${fish.food}</div>
-        </section>
+        <div class="fish">
+            <div class="fishContent">
+                <li class="fish__name">Name: ${fishObj.name}</li>
+                <img class="fish__image"
+                    src="${fishObj.image}"
+                    alt="Photo of a betta fish.">
+                <ul>
+                    <li class="fish__species">Species: ${fishObj.species}</li>
+                    <li class="fish__location">Location: ${fishObj.location}</li>
+                    <li class="fish__length">Length: ${fishObj.length} inches</li>
+                    <li class="fish__diet">Diet: ${fishObj.diet}</li>
+                </ul>
+            </div>
+        </div>
     `
 }
--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,24 +1,33 @@
+import { Fish } from "./Fish.js"
 // FishList which renders individual fish objects as HTML
 
-// TODO: Import `useFish` from the data provider module: DONE
+
+
 import { sliceFishCollection } from "./FishDataProvider.js"
 
 export const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
     const contentElement = document.querySelector(".contentContainer-left")
+    
     const fishes = sliceFishCollection()
 
-    // Add to the existing HTML in the content element
+    let fishHTMLRepresentations = ""
+    for (const fish of fishes) {
+        fishHTMLRepresentations += Fish(fish)
+    }
+
     contentElement.innerHTML += `
     <section class="fishList">
-    <h3 class="container-header">Fish List</h3>
-    <div class="fish">
-        <div class="fishContent">
-        
-        </div>
-    Placeholder for list of fishes
-    </div>
+        <u><h3 class="container-header">Current Collection</h3></u>
+        <ol class="fishnumber">
+            <div class="fish">
+                <div class="fishContent">
+            </div>
+            Placeholder for list of fishes
+            ${fishHTMLRepresentations}
+            </div>
+        </ol>
     </section>
     `
 }
